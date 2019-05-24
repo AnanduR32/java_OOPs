02404e7e51bbc77bf6e3162f2eca5315c626b5ef
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2019-05-24 17:03:09 +0530

    Update SimpleCalc.java

--- Swing/SimpleCalc/V2/SimpleCalc.java
+++ Swing/SimpleCalc/V2/SimpleCalc.java
@@ -1,17 +1,22 @@
+//importing all necessary packages
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.Event;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+//creating class with ActionListener interface implemented
 public class SimpleCalc implements ActionListener{
+    //declaring all the components and the variables
     JTextField t1,t2,t3;
+    //o[] contains combobox items
     String o[]={"Add","Mul","Sub","Div"};
     JComboBox cb;
     JButton b;
     JLabel l1,l2,l3;
+    //constructor of the class SimpleCalc
     SimpleCalc(){
-        
+        //object/components initialized
         t1=new JTextField();
         t2=new JTextField();
         t3=new JTextField();
@@ -22,8 +27,11 @@ public class SimpleCalc implements ActionListener{
         b=new JButton("Calculate");
         JFrame f=new JFrame("Simple Calculator");
         JPanel p=new JPanel();
+        //creating a 4 x 2 grid style layout object
         GridLayout grid=new GridLayout(4,2);
+        //setting the grid layout to the panel 'p'
         p.setLayout(grid);
+        //adding components to the panel
         p.add(l1);
         p.add(t1);
         p.add(l2);
@@ -32,10 +40,13 @@ public class SimpleCalc implements ActionListener{
         p.add(b);
         p.add(l3);
         p.add(t3);
-        
+        //registering the button as an EventListener
         b.addActionListener(this);
+        //adding panel to the frame
         f.setContentPane(p);
+        //setting dimensions of the frame
         f.setSize(400,500);
+        //making frame visible
         f.setVisible(true);
     }
     public void actionPerformed(ActionEvent ae){
@@ -61,6 +72,7 @@ public class SimpleCalc implements ActionListener{
         System.exit(0);
     }
     public static void main(String[] args){
+        //run program
         new SimpleCalc();
     }
 }
